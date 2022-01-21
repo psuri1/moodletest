3595f6b23db0d82d2dc583efb519cdeff44771b9
Punit Suri <smasih@syllametrics.com>
2022-01-21 12:51:08 +0530

    Innitial commit The code will display only course activities The plugin can only be available to the course page

---
+++ block_moodle_test.php
@@ -0,0 +1,62 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Moodle Test block.
+ *
+ * @package   block_moodle_test
+ * @copyright 2022 onwards Punit Suri  {@link http://moodle.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+class block_moodle_test extends block_list {
+
+    public function init() {
+        $this->title = get_string('pluginname', 'block_moodle_test');
+    }
+
+    public function get_content() {
+        global $CFG, $DB, $USER;
+
+        if ($this->content !== null) {
+            return $this->content;
+        }
+
+        $this->content = new stdClass;
+        $course = $this->page->course;
+        $modinfo = get_fast_modinfo($course);
+
+        foreach ($modinfo->cms as $cm) {
+            if (!$cm->uservisible or !$cm->has_view()) {
+                continue;
+            }
+            $queryparams = array('coursemoduleid' => $cm->id, 'userid' => $USER->id);
+            if ($compdata = $DB->get_record('course_modules_completion', $queryparams)) {
+                if ($compdata->completionstate) {
+                    $comletion = ' - Completed';
+                }
+            }
+            $date = date('d-M-Y', $cm->added);
+            $anchor = "<a href='$CFG->wwwroot/mod/$cm->modname/view.php?id=$cm->id'>$cm->id - $cm->name - $date $comletion</a>";
+            $this->content->items[] = $anchor;
+        }
+        return $this->content;
+    }
+
+    public function applicable_formats() {
+        return array('course-view' => true);
+    }
+}
