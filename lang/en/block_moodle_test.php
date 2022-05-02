ecdfdc08c3db197df8ebbda398113dce54bff1ae
Punit Suri <smasih@syllametrics.com>
2022-05-02 14:01:07 +0530

    code updated

---
+++ lang/en/block_moodle_test.php
@@ -0,0 +1,26 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'block_moodle_test', language 'en'
+ *
+ * @package   block_moodle_test
+ * @copyright 2022 onwards Punit Suri  {@link http://moodle.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['moodle_test:addinstance'] = 'Add a new moodle test block';
+$string['pluginname'] = 'Moodle Test';
